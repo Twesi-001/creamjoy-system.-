057646e65e9f678131413f4bd8ef9db5ac89b804
Twesi-001 <2023bse147@std.must.ac.ug>
2026-06-29 21:33:54 +0300

    modified models.py

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -2,5 +2,18 @@ from flask import Blueprint
 
 api_bp = Blueprint('api', __name__, url_prefix='/api')
 
-from . import auth, batches, products,raw_materials,suppliers, orders, deliveries, inventory, customers, expenditures, credit_accounts
-from . import test 
+from . import auth
+from . import batches
+from . import products
+from . import raw_materials
+from . import suppliers
+from . import orders
+from . import deliveries
+from . import inventory
+from . import customers
+from . import expenditures
+from . import credit_accounts
+from . import admin
+from . import test
+
+print("✅ All routes registered!")
